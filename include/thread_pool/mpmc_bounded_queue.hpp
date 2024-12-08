5e17d5c61606c19f489c0825979c792b41145a91
Yuri Voinov <yvoinov@gmail.com>
2024-12-08 21:01:50 +0500

    Update mpmc_bounded_queue.hpp
    Refactored lib/thread_pool/mpmc_bounded_queue.hpp.

--- include/thread_pool/mpmc_bounded_queue.hpp
+++ include/thread_pool/mpmc_bounded_queue.hpp
@@ -191,13 +191,13 @@ inline bool MPMCBoundedQueue<T>::push(U&& data)
     for (;;)
     {
         cell = &m_buffer[pos & m_buffer_mask];
-        std::intptr_t dif = (std::intptr_t)(cell->sequence.load(std::memory_order_acquire)) - (std::intptr_t)pos;
-        if (dif == 0)
+        std::ptrdiff_t diff = static_cast<std::ptrdiff_t>(cell->sequence.load(std::memory_order_acquire)) - static_cast<std::ptrdiff_t>(pos);
+        if (diff == 0)
         {
             if(m_enqueue_pos.compare_exchange_weak(pos, pos + 1, std::memory_order_acq_rel))
                 break;
         }
-        else if(dif < 0)
+        else if (diff < 0)
         {
             return false;
         }
@@ -222,13 +222,13 @@ inline bool MPMCBoundedQueue<T>::pop(T& data)
     for (;;)
     {
         cell = &m_buffer[pos & m_buffer_mask];
-        std::intptr_t dif = (std::intptr_t)(cell->sequence.load(std::memory_order_acquire)) - (std::intptr_t)(pos + 1);
-        if (dif == 0)
+        std::ptrdiff_t diff = static_cast<std::ptrdiff_t>(cell->sequence.load(std::memory_order_acquire)) - static_cast<std::ptrdiff_t>(pos + 1);
+        if (diff == 0)
         {
             if (m_dequeue_pos.compare_exchange_weak(pos, pos + 1, std::memory_order_acq_rel))
                 break;
         }
-        else if(dif < 0)
+        else if (diff < 0)
         {
             return false;
         }
